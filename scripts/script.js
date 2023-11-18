6f95a4ed151ee86c544429d017a9bb4e894aedc0
chrisraupp <chrisrauppr@hotmail.com>
2023-11-17 23:18:54 -0300

    atualizado arquivo script e deletado html dos forms antigos

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,73 @@
-// const form1 = document.getElementById("form1");
-// const nome = document.getElementById("nome");
-// const profissional = document.getElementById("profissional");
-// const data = document.getElementById("data");
-// const peso = document.getElementById("peso");
-// const altura = document.getElementById("altura");
-// const idade = document.getElementById("idade");
+// teste de pagina step 
+ alert("loaded");
+ var form1 = document.getElementById("form1");
 
-// form1.addEventListener("submit",(event)=>{
-//     event.preventDefault();
+// espera o submit do botao salvar dados e realizar o fluxo
+form1.addEventListener("submit", (e) =>{
 
-//     getValoresForm1();
-// })
+    // nao atualiza a pagina para nao perder as infos
+    e.preventDefault();
+    // teste de pagina step
+    console.log("salvar");
 
-// function getValoresForm1(){
-//     const nomeValue = nome.value;
-//     const profissionalValue = profissional.value;
-//     const dataValue = data.value;
-//     const pesoValue = peso.value;
-//     const alturaValue = altura.value;
-//     const idadeValue = idade.value;
-//     console.log(nomeValue);
+    
+    // variaveis dos inputs
+    var nome = document.getElementById("nome").value;
+    var profissional = document.getElementById("profissional").value;
+    var data = document.getElementById("data").value;
+    var peso = document.getElementById("peso").value;
+    var altura = document.getElementById("altura").value;
+    var idade = document.getElementById("idade").value;
+
+    var dobraBicipesETricipes = document.getElementById("dobraBicipesETricipes").value;
+    var dobraSuprailiaca = document.getElementById("dobraSuprailiaca").value;
+    var dobraSubescapular = document.getElementById("dobraSubescapular").value;
+    var dobraAuxiliar = document.getElementById("dobraAuxiliar").value;
+    var cobraAbdominal = document.getElementById("cobraAbdominal").value;
+    var dobraCoxa = document.getElementById("dobraCoxa").value;
+    var dobraPanturrilha = document.getElementById("dobraPanturrilha").value;
+
+    var medidaTorax = document.getElementById("medidaTorax").value;
+    var medidaBracos = document.getElementById("medidaBracos").value;
+    var medidaCintura = document.getElementById("medidaCintura").value;
+    var medidaAbdomem = document.getElementById("medidaAbdomem").value;
+    var medidaQuadril = document.getElementById("medidaQuadril").value;
+    var medidaCoxas = document.getElementById("medidaCoxas").value;
+    var medidaGemeos = document.getElementById("medidaGemeos").value;
+    
+    
+    // testar console *NAO ESQUECER*
+    console.log(nome);
+    console.log(profissional);
+    console.log(data);
+    console.log(peso);
+    console.log(altura);
+    console.log(idade);
+    // cria Array de Objetos
+    let usuarios = new Array();
+    
+    // verificar se tem items no localstorage
+    if(localStorage.hasOwnProperty("usuarios")){
+        //recupera os itens em string no localstorage e converte pra objetos
+        usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    }
+    // adiciona os dados na variavel usuarios com os dados dos input
+    usuarios.push({nome, profissional, data, peso, altura, idade, dobraBicipesETricipes, dobraSuprailiaca, dobraSubescapular, dobraAuxiliar, cobraAbdominal, dobraCoxa, dobraPanturrilha, medidaTorax, medidaCintura, medidaAbdomem, medidaQuadril, medidaCoxas, medidaGemeos
+     });
+    console.log(usuarios);
+    
+    //converte o objeto em string
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    
+    
+});
 
 
-// }
+
+
+// regra imc = peso/(altura*altura)
+function calcularIMC(peso, altura) {
+    const alturaMetros = altura/100;
+    const imc = peso / (alturaMetros * alturaMetros);
+    return imc;
+  }
